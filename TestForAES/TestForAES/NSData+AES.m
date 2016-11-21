3f63432bb96c8c37c4f457d5a1f19764af9ba476
weskhen <wujian516411567@163.com>
2016-11-21 10:47:37 +0800

    Update
    Update

--- TestForAES/TestForAES/NSData+AES.m
+++ TestForAES/TestForAES/NSData+AES.m
@@ -25,7 +25,7 @@
 
 - (NSData *)AESOperation:(CCOperation)operation key:(NSString *)key iv:(NSString *)iv padding:(BOOL)padding
 {
-    //kCCKeySizeAES256 32位加密  kCCKeySizeAES128 16位加密 kCCKeySizeAES192 24位加密 这边测试的CBC模式
+    //kCCKeySizeAES256 32位加密  kCCKeySizeAES128 16位加密 kCCKeySizeAES192 24位加密 这边测试的ECB CBC模式
     char keyPtr[kCCKeySizeAES256 + 1];
     bzero(keyPtr, sizeof(keyPtr));
     [key getCString:keyPtr maxLength:sizeof(keyPtr) encoding:NSUTF8StringEncoding];
@@ -51,7 +51,7 @@
     void *buffer = malloc(bufferSize);
 
     size_t numBytesEncrypted = 0;
-    CCCryptorStatus cryptorStatus = CCCrypt(operation, kCCAlgorithmAES128, padding?kCCOptionPKCS7Padding:0,
+    CCCryptorStatus cryptorStatus = CCCrypt(operation, kCCAlgorithmAES128, padding?kCCOptionPKCS7Padding:kCCOptionECBMode,
                                             keyPtr, kCCKeySizeAES256,
                                             ivValue, //初始向量
                                             rawbytes, dataLength, //输入
